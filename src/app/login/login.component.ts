06b5c1579d6198b7ee0af02a1c8a7c62d78a9b97
Jeevan K S <jevansathyan4u@gmail.com>
2022-07-18 11:48:25 +0530

    template refference

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,7 @@ public acstr='enter ypur account number here'
 acno=''
 pswd=''
 
-userDetails={
+userDetails:any={
 1000:{acno:1000,userName:'jeevan',password:1000,balence:100000},
 1001:{acno:1001,userName:'akhil',password:1001,balence:230000},
 1002:{acno:1002,userName:'amith',password:1002,balence:340000},
@@ -24,17 +24,31 @@ userDetails={
   ngOnInit(): void {
   }
 //here we can define user defined functions
-login(){
-if(this.acno in this.userDetails){
-if(this.acno==this.userDetails)
+// login(){
+// if(this.acno in this.userDetails){
+// console.log(this.userDetails)
+// }
+// }
+// pswdChange(event:any){
+// this.pswd=event.target.value;
+// console.log(this.pswd)
+// }
+// acnoChange(event:any){
+// this.acno=event.target.value;
+// console.log(this.acno)
+// }
+login(a:any,b:any){
+var acno=a.value
+var pswd=b.value
+
+let userDetails=this.userDetails
+  if(acno in userDetails){
+  if(pswd==userDetails[acno]['password']){
+        alert('login success');}
+    else{alert('incorrect password')}
+    }
+      else{alert('user doesnot exist')
 }
 }
-pswdChange(event:any){
-this.pswd=event.target.value;
-console.log(this.pswd)
-}
-acnoChange(event:any){
-this.acno=event.target.value;
-console.log(this.acno)
-}
+
 }
